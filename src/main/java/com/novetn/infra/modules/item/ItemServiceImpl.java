79eadeccfc9db9ae8bb5004a9fed813cd0b3c74f
jjjy879 <jjjy879@naver.com>
2022-09-19 18:52:55 +0900

    .

--- src/main/java/com/novetn/infra/modules/item/ItemServiceImpl.java
+++ src/main/java/com/novetn/infra/modules/item/ItemServiceImpl.java
@@ -3,6 +3,7 @@ package com.novetn.infra.modules.item;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 @Service
 public class ItemServiceImpl implements ItemService{
