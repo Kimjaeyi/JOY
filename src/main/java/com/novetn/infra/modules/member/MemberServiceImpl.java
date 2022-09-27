050bf9d0bc01e5fcba901b2af7502f3c45db1db0
jjjy879 <jjjy879@naver.com>
2022-09-27 13:52:44 +0900

    .

--- src/main/java/com/novetn/infra/modules/member/MemberServiceImpl.java
+++ src/main/java/com/novetn/infra/modules/member/MemberServiceImpl.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.novetn.infra.common.util.UtilSecurity;
+
 @Service
 public class MemberServiceImpl implements MemberService {
 
@@ -19,6 +21,7 @@ public class MemberServiceImpl implements MemberService {
 	
 	@Override
 	public int insert(Member dto) throws Exception {
+		dto.setPwd(UtilSecurity.encryptSha256(dto.getPwd()));
 		int result = dao.insert(dto);
 		System.out.println("service result : " + result);
 		return result;
