be2717b2f5a65e4473d0c31773ed21b130d00ead
jjjy879 <jjjy879@naver.com>
2022-09-28 17:16:15 +0900

    .

--- src/main/java/com/novetn/infra/common/interceptor/CheckLoginSessionInterception.java
+++ src/main/java/com/novetn/infra/common/interceptor/CheckLoginSessionInterception.java
@@ -12,12 +12,12 @@ public class CheckLoginSessionInterception extends HandlerInterceptorAdapter {
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 		if (request.getSession().getAttribute("sessSeq") != null) {
-			System.out.println("CheckLoginSessionInterception is running :)");
 			// by pass
 		} else {
 			response.sendRedirect(Constants.URL_LOGINFORM);
             return false;
 		}
+		System.out.println("CheckLoginSessionInterception is running :)");
 		return super.preHandle(request, response, handler);
 	}
 	
