3fde8a11bbfd9741238124bb71750fd746e398f7
jjjy879 <jjjy879@naver.com>
2022-10-12 11:10:26 +0900

    .

--- src/main/java/com/novetn/infra/common/base/Base.java
+++ src/main/java/com/novetn/infra/common/base/Base.java
@@ -1,5 +1,191 @@
 package com.novetn.infra.common.base;
 
+import java.util.Date;
+
+import org.springframework.web.multipart.MultipartFile;
+
 public class Base {
 
+//	uploaded
+	private String tableName;
+	
+	private String seq;
+	private Integer type;
+	private Integer defaultNY;
+	private Integer sort;
+	private String path;
+	private String originalName;
+	private String uuidName;
+	private String ext;
+	private long size;
+	private Integer delNY;
+	private String pseq;
+	
+	private MultipartFile[] MultipartFile;
+	
+//	regmod
+	private String regIp;
+	private String regSeq;
+	private String regId;
+	private Integer regDeviceCd;
+	private Date regDateTime;
+	private Date regDateTimeSvr;
+	private String modIp;
+	private String modSeq;
+	private String modId;
+	private Integer modDeviceCd;
+	private Date modDateTime;
+	private Date modDateTimeSvr;
+	
+	public String getTableName() {
+		return tableName;
+	}
+	public void setTableName(String tableName) {
+		this.tableName = tableName;
+	}
+	public String getSeq() {
+		return seq;
+	}
+	public void setSeq(String seq) {
+		this.seq = seq;
+	}
+	public Integer getType() {
+		return type;
+	}
+	public void setType(Integer type) {
+		this.type = type;
+	}
+	public Integer getDefaultNY() {
+		return defaultNY;
+	}
+	public void setDefaultNY(Integer defaultNY) {
+		this.defaultNY = defaultNY;
+	}
+	public Integer getSort() {
+		return sort;
+	}
+	public void setSort(Integer sort) {
+		this.sort = sort;
+	}
+	public String getPath() {
+		return path;
+	}
+	public void setPath(String path) {
+		this.path = path;
+	}
+	public String getOriginalName() {
+		return originalName;
+	}
+	public void setOriginalName(String originalName) {
+		this.originalName = originalName;
+	}
+	public String getUuidName() {
+		return uuidName;
+	}
+	public void setUuidName(String uuidName) {
+		this.uuidName = uuidName;
+	}
+	public String getExt() {
+		return ext;
+	}
+	public void setExt(String ext) {
+		this.ext = ext;
+	}
+	public long getSize() {
+		return size;
+	}
+	public void setSize(long size) {
+		this.size = size;
+	}
+	public Integer getDelNY() {
+		return delNY;
+	}
+	public void setDelNY(Integer delNY) {
+		this.delNY = delNY;
+	}
+	public String getPseq() {
+		return pseq;
+	}
+	public void setPseq(String pseq) {
+		this.pseq = pseq;
+	}
+	public String getRegIp() {
+		return regIp;
+	}
+	public void setRegIp(String regIp) {
+		this.regIp = regIp;
+	}
+	public String getRegSeq() {
+		return regSeq;
+	}
+	public void setRegSeq(String regSeq) {
+		this.regSeq = regSeq;
+	}
+	public String getRegId() {
+		return regId;
+	}
+	public void setRegId(String regId) {
+		this.regId = regId;
+	}
+	public Integer getRegDeviceCd() {
+		return regDeviceCd;
+	}
+	public void setRegDeviceCd(Integer regDeviceCd) {
+		this.regDeviceCd = regDeviceCd;
+	}
+	public Date getRegDateTime() {
+		return regDateTime;
+	}
+	public void setRegDateTime(Date regDateTime) {
+		this.regDateTime = regDateTime;
+	}
+	public Date getRegDateTimeSvr() {
+		return regDateTimeSvr;
+	}
+	public void setRegDateTimeSvr(Date regDateTimeSvr) {
+		this.regDateTimeSvr = regDateTimeSvr;
+	}
+	public String getModIp() {
+		return modIp;
+	}
+	public void setModIp(String modIp) {
+		this.modIp = modIp;
+	}
+	public String getModSeq() {
+		return modSeq;
+	}
+	public void setModSeq(String modSeq) {
+		this.modSeq = modSeq;
+	}
+	public String getModId() {
+		return modId;
+	}
+	public void setModId(String modId) {
+		this.modId = modId;
+	}
+	public Integer getModDeviceCd() {
+		return modDeviceCd;
+	}
+	public void setModDeviceCd(Integer modDeviceCd) {
+		this.modDeviceCd = modDeviceCd;
+	}
+	public Date getModDateTime() {
+		return modDateTime;
+	}
+	public void setModDateTime(Date modDateTime) {
+		this.modDateTime = modDateTime;
+	}
+	public Date getModDateTimeSvr() {
+		return modDateTimeSvr;
+	}
+	public void setModDateTimeSvr(Date modDateTimeSvr) {
+		this.modDateTimeSvr = modDateTimeSvr;
+	}
+	public MultipartFile[] getMultipartFile() {
+		return MultipartFile;
+	}
+	public void setMultipartFile(MultipartFile[] multipartFile) {
+		MultipartFile = multipartFile;
+	}
+	
 }
