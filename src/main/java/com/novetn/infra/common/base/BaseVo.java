6b04772015710e6b9a19c3d049c7fe992324f0b6
jjjy879 <jjjy879@naver.com>
2022-10-12 11:10:32 +0900

    .

--- src/main/java/com/novetn/infra/common/base/BaseVo.java
+++ src/main/java/com/novetn/infra/common/base/BaseVo.java
@@ -4,8 +4,6 @@ import com.novetn.infra.common.constants.Constants;
 
 public class BaseVo {
 
-	private String mainKey;
-	
 //	paging
 	private int thisPage = 1;									// 현재 페이지
 	private int rowNumToShow = Constants.ROW_NUM_TO_SHOW;		// 화면에 보여줄 데이터 줄 갯수
@@ -22,5 +20,221 @@ public class BaseVo {
 
 	private int startRnumForMysql = 0;							// 쿼리 시작 row
 	
+	
+//	search
+	private Integer shUseNy;
+	private Integer shDelNy;
+	private Integer shOptionDate;
+	private String shDateStart;
+	private String shDateEnd;
+	private Integer shOption;
+	private String ShValue;
+
+	
+//	list
+	private String[] checkboxSeqArray;
+	
+//	----------
+	public void setParamsPaging(int totalRows) {
+		
+//		setThisPage(3);
+
+		setTotalRows(totalRows);
+
+		if (getTotalRows() == 0) {
+			setTotalPages(1);
+		} else {
+			setTotalPages(getTotalRows() / getRowNumToShow());
+		}
+
+		if (getTotalRows() % getRowNumToShow() > 0) {
+			setTotalPages(getTotalPages() + 1);
+		}
+
+		if (getTotalPages() < getThisPage()) {
+			setThisPage(getTotalPages());
+		}
+		
+		setStartPage(((getThisPage() - 1) / getPageNumToShow()) * getPageNumToShow() + 1);
+
+		setEndPage(getStartPage() + getPageNumToShow() - 1);
+
+		if (getEndPage() > getTotalPages()) {
+			setEndPage(getTotalPages());
+		}
+		
+		setEndRnumForOracle((getRowNumToShow() * getThisPage()));
+		setStartRnumForOracle((getEndRnumForOracle() - getRowNumToShow()) + 1);
+		if (getStartRnumForOracle() < 1) setStartRnumForOracle(1);
+		
+		if (thisPage == 1) {
+			setStartRnumForMysql(0);
+		} else {
+			setStartRnumForMysql((getRowNumToShow() * (getThisPage()-1)));
+		}
+		
+//		System.out.println("getThisPage():" + getThisPage());
+//		System.out.println("getTotalRows():" + getTotalRows());
+//		System.out.println("getRowNumToShow():" + getRowNumToShow());
+//		System.out.println("getTotalPages():" + getTotalPages());
+//		System.out.println("getStartPage():" + getStartPage());
+//		System.out.println("getEndPage():" + getEndPage());		
+//		System.out.println("getStartRnumForOracle():" + getStartRnumForOracle());
+//		System.out.println("getEndRnumForOracle():" + getEndRnumForOracle());
+//		System.out.println("getStartRnumForMysql(): " + getStartRnumForMysql());
+		
+	}
+
+	public int getThisPage() {
+		return thisPage;
+	}
+
+	public void setThisPage(int thisPage) {
+		this.thisPage = thisPage;
+	}
+
+	public int getRowNumToShow() {
+		return rowNumToShow;
+	}
+
+	public void setRowNumToShow(int rowNumToShow) {
+		this.rowNumToShow = rowNumToShow;
+	}
+
+	public int getPageNumToShow() {
+		return pageNumToShow;
+	}
+
+	public void setPageNumToShow(int pageNumToShow) {
+		this.pageNumToShow = pageNumToShow;
+	}
+
+	public int getTotalRows() {
+		return totalRows;
+	}
+
+	public void setTotalRows(int totalRows) {
+		this.totalRows = totalRows;
+	}
+
+	public int getTotalPages() {
+		return totalPages;
+	}
+
+	public void setTotalPages(int totalPages) {
+		this.totalPages = totalPages;
+	}
+
+	public int getStartPage() {
+		return startPage;
+	}
+
+	public void setStartPage(int startPage) {
+		this.startPage = startPage;
+	}
+
+	public int getEndPage() {
+		return endPage;
+	}
+
+	public void setEndPage(int endPage) {
+		this.endPage = endPage;
+	}
+
+	public int getStartRnumForOracle() {
+		return startRnumForOracle;
+	}
+
+	public void setStartRnumForOracle(int startRnumForOracle) {
+		this.startRnumForOracle = startRnumForOracle;
+	}
+
+	public int getEndRnumForOracle() {
+		return endRnumForOracle;
+	}
+
+	public void setEndRnumForOracle(int endRnumForOracle) {
+		this.endRnumForOracle = endRnumForOracle;
+	}
+
+	public Integer getRNUM() {
+		return RNUM;
+	}
+
+	public void setRNUM(Integer rNUM) {
+		RNUM = rNUM;
+	}
+
+	public int getStartRnumForMysql() {
+		return startRnumForMysql;
+	}
+
+	public void setStartRnumForMysql(int startRnumForMysql) {
+		this.startRnumForMysql = startRnumForMysql;
+	}
+
+	public Integer getShUseNy() {
+		return shUseNy;
+	}
+
+	public void setShUseNy(Integer shUseNy) {
+		this.shUseNy = shUseNy;
+	}
+
+	public Integer getShDelNy() {
+		return shDelNy;
+	}
+
+	public void setShDelNy(Integer shDelNy) {
+		this.shDelNy = shDelNy;
+	}
+
+	public Integer getShOptionDate() {
+		return shOptionDate;
+	}
+
+	public void setShOptionDate(Integer shOptionDate) {
+		this.shOptionDate = shOptionDate;
+	}
+
+	public String getShDateStart() {
+		return shDateStart;
+	}
+
+	public void setShDateStart(String shDateStart) {
+		this.shDateStart = shDateStart;
+	}
+
+	public String getShDateEnd() {
+		return shDateEnd;
+	}
+
+	public void setShDateEnd(String shDateEnd) {
+		this.shDateEnd = shDateEnd;
+	}
+
+	public Integer getShOption() {
+		return shOption;
+	}
+
+	public void setShOption(Integer shOption) {
+		this.shOption = shOption;
+	}
+
+	public String getShValue() {
+		return ShValue;
+	}
+
+	public void setShValue(String shValue) {
+		ShValue = shValue;
+	}
+
+	public String[] getCheckboxSeqArray() {
+		return checkboxSeqArray;
+	}
+
+	public void setCheckboxSeqArray(String[] checkboxSeqArray) {
+		this.checkboxSeqArray = checkboxSeqArray;
+	}
 	
 }
