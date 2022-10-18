b5f8feabddef8e1b235399761b52519fd83138b3
jjjy879 <jjjy879@naver.com>
2022-10-18 16:22:18 +0900

    .

--- src/main/java/com/novetn/infra/modules/item/ItemService.java
+++ src/main/java/com/novetn/infra/modules/item/ItemService.java
@@ -10,6 +10,7 @@ import java.util.List;
 	public List<Item> selectHotdealwmp(ItemVo vo) throws Exception;
 	public List<Item> selectHotdealtmon(ItemVo vo) throws Exception;
 	public List<Item> selectHotdeal11st(ItemVo vo) throws Exception;
+	public List<Item> selectBestlist(ItemVo vo) throws Exception;
 	
 //	이미지 업로드
 	public List<Item> selectUploadedimg(ItemVo vo) throws Exception;
